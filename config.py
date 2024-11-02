411dcb485cf6f8394b0d0108bc973e29fe160f1b
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-11-02 15:26:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ load_dotenv()
 API_ID = int(getenv("API_ID","23342470"))
 API_HASH = getenv("API_HASH","824d7edf65f29a3bb7125173ff8ca9b0")
 BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID","6407861676"))
+OWNER_ID = int(getenv("OWNER_ID","5738579437"))
 
 MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://aradhyamusicbot:2QWdrmNlRVYovcVq@cluster0.ekjld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
