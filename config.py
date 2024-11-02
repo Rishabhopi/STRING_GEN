8a47a72b2666a0179b5efa6376842b3fb87ca4e3
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-11-02 15:26:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,4 @@ API_HASH = getenv("API_HASH","824d7edf65f29a3bb7125173ff8ca9b0")
 BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_ID = int(getenv("OWNER_ID","6407861676"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://BABYTUNE7654:<BABYTUNE7654>@cluster0.i7r8ucr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-MUST_JOIN = getenv("MUST_JOIN","BABY09_WORLD")
+MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://aradhyamusicbot:2QWdrmNlRVYovcVq@cluster0.ekjld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
